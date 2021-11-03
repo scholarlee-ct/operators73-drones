f971bd60e1251867b4e8b4c16ddced0941c92f7b
nwelter1 <nwelter1@gmail.com>
2021-11-03 12:25:08 -0500

    day3-morning

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,1 +1,58 @@
-let token = `YOUR API TOKEN GOES HERE`
+const token = '72f1cc607dbe687feee1b93911602467dbc134b63e5201d0';
+
+
+export const server_calls = {
+    get: async () => {
+        const response = await fetch('https://operators-drones.herokuapp.com/api/drones',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+        if (!response.ok){
+            throw new Error('Failed to fetch your data from the server...')
+        }
+        return await response.json()
+    },
+    create: async (data: any = {}) =>{
+        const response = await fetch('https://operators-drones.herokuapp.com/api/drones', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok){
+            throw new Error('Failed to create your drone...')
+        }
+        return await response.json()
+    },
+    update: async (id:string, data: any = {}) => {
+        const response = await fetch(`https://operators-drones.herokuapp.com/api/drones/${id}`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok){
+            throw new Error('Failed to update your drone...')
+        }
+        return await response.json()
+    },
+    delete: async(id:string) =>{
+        const response = await fetch(`https://operators-drones.herokuapp.com/api/drones/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+        if (!response.ok){
+            throw new Error('Could not delete your drone...')
+        }
+    }
+}
